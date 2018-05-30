6e040a27af01f89b5a4fc7bbcd24984ffceaa8e1
Christoph Schorn <christoph.schorn@gmail.com>
2018-05-30 17:46:01 +0200

    Show git branch in commandline

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -2,7 +2,11 @@
 if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
     debian_chroot=$(cat /etc/debian_chroot)
 fi
+# Add git branch if its present to PS1
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
 
 # Sets the prompt colors and text (just assume colors are possible...)
 # Otherwise maybe use if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null;  etc.
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(parse_git_branch)\[\033[00m\]\$ '
