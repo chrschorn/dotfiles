60ac1cc316646a2f0b1094f7665fecf9297b4084
Christoph Schorn <christoph.schorn@gmail.com>
2019-04-03 12:28:35 +0200

    Add time to command prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -9,4 +9,4 @@ parse_git_branch() {
 
 # Sets the prompt colors and text (just assume colors are possible...)
 # Otherwise maybe use if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null;  etc.
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(parse_git_branch)\[\033[00m\]\$ '
+PS1='[\D{%m-%d %T}] ${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(parse_git_branch)\[\033[00m\]\$ '
